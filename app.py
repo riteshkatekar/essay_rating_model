16136500126e19a62b577431089eab340959056b
ritesh <ritesh@Rits.>
2025-03-05 10:24:00 +0000

    Added all files

--- app.py
+++ app.py
@@ -7,12 +7,16 @@ import textstat
 from nltk.tokenize import word_tokenize, sent_tokenize
 from nltk.sentiment import SentimentIntensityAnalyzer
 
-# Ensure NLTK data is downloaded and set proper paths
-nltk_data_path = "/usr/share/nltk_data"
+
+# Set NLTK data directory inside the project folder
+nltk_data_path = os.path.join(os.getcwd(), "nltk_data")
 os.makedirs(nltk_data_path, exist_ok=True)
 nltk.data.path.append(nltk_data_path)
+
+# Download required NLTK resources
 nltk.download('punkt', download_dir=nltk_data_path)
 nltk.download('vader_lexicon', download_dir=nltk_data_path)
+
 
 # Initialize NLP tools
 sia = SentimentIntensityAnalyzer()
